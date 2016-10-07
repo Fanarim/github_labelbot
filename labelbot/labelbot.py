5ef443482cfcadcb9314d93d40522e056b7784b0
David Viktora <viktora.david@gmail.com>
2016-10-07 19:09:47 +0200

    Add token parsing and request session initialisation

--- labelbot/labelbot.py
+++ labelbot/labelbot.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python3
 
-import click
+import requests
+import sys
 
 
 class LabelBot(object):
+    def __init__(self):
+        self.last_issue_checked = 0
 
-    @click.command()
-    @click.option('--token-file',
-                  '-t',
-                  type=click.Path(),
-                  help='file containing GitHub token information')
-    @click.option('--repo',
-                  '-r',
-                  help='URL of repository this bot should operate on')
-    @click.option('--rules-file',
-                  '-u',
-                  type=click.Path(),
-                  help='file containing issues labeling rules')
-    @click.option('--interval',
-                  '-i',
-                  type=int,
-                  help='time interval in seconds in which to check issues')
-    @click.option('--default-label',
-                  '-d',
-                  help='default label to use after no rule is matched')
-    @click.option('--check-comments',
-                  '-c',
-                  is_flag=True,
-                  help='flag indicating comments should also be checked')
-    def run(repo, token_file, rules_file, interval, default_label,
-            check_comments):
-        """Run the command"""
-        click.echo('Hello {}!'.format(token_file))
-        print('Hello {}!'.format(token_file))
+    def run(self, repo, token_file, rules_file, interval, default_label,
+            check_comments, recheck):
+        """Run the labelbot"""
+        token = self._get_token(token_file)
+        self.session = self._get_requests_session(token)
+        # periodically label issues
+
+    def _get_token(self, token_file):
+        """Get GitHub token from the provided file"""
+        with open(token_file) as token_file:
+            token = token_file.readline().splitlines()[0]
+
+        return token
+
+    def _get_requests_session(self, token):
+        """Returns a requests session and verifies valid GitHub token was
+        provided"""
+        session = requests.Session()
+        session.headers = {'Authorization': 'token ' + token,
+                           'User-Agent': 'Python'}
+        try:
+            response = session.get('https://api.github.com/user')
+        except:
+            print('Could not connect to GitHub. Are you online? ')
+            sys.exit(1)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print('Couldn\'t connect to GitHub: ' +
+                  str(response.status_code) +
+                  ' - ' +
+                  response.reason,
+                  file=sys.stderr)
+
+            if response.status_code == 401:
+                print('Did you provide a valid token? ',
+                      file=sys.stderr)
+
+            sys.exit(1)
+
+        return session
--- labelbot/main.py
+++ labelbot/main.py
@@ -1,7 +1,49 @@
 #!/usr/bin/env python3
 
+import click
+
 from labelbot import LabelBot
 
 
+@click.command()
+@click.option('--token-file',
+              '-t',
+              type=click.Path(exists=True,
+                              file_okay=True,
+                              readable=True),
+              default='token.cfg',
+              help='file containing GitHub token information')
+@click.option('--repo',
+              '-r',
+              help='URL of repository this bot should operate on')
+@click.option('--rules-file',
+              '-u',
+              type=click.Path(exists=True,
+                              file_okay=True,
+                              readable=True),
+            #   default='rules.cfg',
+              help='file containing issues labeling rules')
+@click.option('--interval',
+              '-i',
+              type=int,
+              help='time interval in seconds in which to check issues')
+@click.option('--default-label',
+              '-d',
+              help='default label to use after no rule is matched')
+@click.option('--check-comments',
+              '-c',
+              is_flag=True,
+              help='flag indicating comments should also be checked')
+@click.option('--recheck',
+              '-r',
+              is_flag=True,
+              help='flag indicating all issues should be checked, \
+                    not only the new ones')
+def cli(repo, token_file, rules_file, interval, default_label,
+        check_comments, recheck):
+    labelbot = LabelBot()
+    labelbot.run(repo, token_file, rules_file, interval, default_label,
+                 check_comments, recheck)
+
 if __name__ == '__main__':
-    LabelBot.run()
+    cli()
